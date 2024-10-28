949c764572adfe8b3c7f1af93a4f5fd1cafe355d
Henrique Silva <up202105647@up.pt>
2024-10-28 16:53:55 +0000

    added glax box

---
+++ tp1/MyGlassBox.js
@@ -0,0 +1,25 @@
+import * as THREE from 'three';
+
+class MyGlassBox {
+
+    constructor(app, info, position) {
+        this.app = app;
+        this.info = info;
+        this.position = position;
+
+        console.log(this.position);
+    }
+
+    createMesh() {
+        const box = new THREE.BoxGeometry(this.info.width, this.info.height, this.info.depth);
+
+        const padding = this.info.height / 2;
+
+        const mesh = new THREE.Mesh(box, this.info.material);
+        mesh.position.set(this.position[0], this.position[1] + padding, this.position[2]);
+
+        return mesh;
+    }
+}
+
+export { MyGlassBox };
