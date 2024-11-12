67b149c9f86e310ff1a12053a3080e1ea7f32980
Henrique Silva <up202105647@up.pt>
2024-11-12 23:35:09 +0000

    added primitives

--- tp2/MyPrimitive.js
+++ tp2/MyPrimitive.js
@@ -1,31 +1,94 @@
 import * as THREE from 'three';
 
 class MyPrimitive {
+
     static getPrimitive(prim) {
+
         switch (prim.subtype) {
             case 'rectangle':
-                this.#getRectangle(prim.representations[0]);
-                break;
+                return MyPrimitive.#getRectangle(prim.representations[0]);
             case 'triangle':
-                break;
+                return MyPrimitive.#getTriangle(prim.representations[0]);
             case 'box':
-                break;
+                return MyPrimitive.#getBox(prim.representations[0]);
             case 'cylinder':
-                break;
+                return MyPrimitive.#getCylinder(prim.representations[0]);
             case 'sphere':
-                break;
+                return MyPrimitive.#getSphere(prim.representations[0]);
             case 'nurbs':
-                break;
+                return MyPrimitive.#getNurbs(prim.representations[0]);
             case 'polygon':
-                break;
+                return MyPrimitive.#getPolygon(prim.representations[0]);
             default:
                 break;
         }
+
     }
 
     static #getRectangle(representation) {
+        const width = Math.abs(representation.xy1[0] - representation.xy2[0]);
+        const height = Math.abs(representation.xy1[1] - representation.xy2[1]);
+        const widthSegments = representation.parts_x;
+        const heightSegments = representation.parts_y;
 
+        const rectangle = new THREE.PlaneGeometry(width, height, widthSegments, heightSegments);
+
+        return rectangle;
     }
+
+    static #getTriangle(representation) {
+        const triangle = new THREE.Triangle(representation.xyz1, representation.xyz2, representation.xyz3);
+        return triangle;
+    }
+
+    static #getBox(representation) {
+        const width = Math.abs(representation.xyz1[0] - representation.xyz2[0]);
+        const height = Math.abs(representation.xyz1[1] - representation.xyz2[1]);
+        const depth = Math.abs(representation.xyz1[2] - representation.xyz2[2]);
+        const widthSegments = representation.parts_x;
+        const heightSegments = representation.parts_y;
+        const depthSegments = representation.parts_z;
+
+        const box = new THREE.BoxGeometry(width, height, depth, widthSegments, heightSegments, depthSegments);
+        
+        return box;
+    }
+
+    static #getCylinder(representation) {
+        const cylinder = new THREE.CylinderGeometry(
+            representation.top,
+            representation.base,
+            representation.height,
+            representation.slices,
+            representation.stacks,
+            representation.capsclose,
+            representation.thetaStart,
+            representation.thetaLength
+        );
+
+        return cylinder;
+    }
+
+    static #getSphere(representation) {
+        const sphere = new THREE.SphereGeometry(
+            representation.radius,
+            representation.slices,
+            representation.stacks,
+            representation.phistart,
+            representation.philength,
+            representation.thetastart,
+            representation.thetalength
+        );
+
+        return sphere;
+    }
+
+    static #getNurbs(representation) {
+    }
+
+    static #getPolygon(representation) {
+    }
+
 }
 
 export { MyPrimitive };
